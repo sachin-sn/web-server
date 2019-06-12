047b7b3a2546cbc57d76678547aea52e7aa7f5c0
Sachin S N <sachin@Sachins-MacBook-Pro.local>
2019-06-12 07:25:37 +0530

    chapter complete

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define path for Express config
 let publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname,'../templates')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
 // Setup handlebars engine and views locations
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to server
 app.use(express.static(publicDirPath))
@@ -17,19 +20,24 @@ app.use(express.static(publicDirPath))
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Gists App',
-    desc:'it is gist app'
+    desc:'it is gist app',
+    dev:"sachin"
   })
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {
-    content:'This is some content coming form app.js'
+    title: 'About',
+    content:'This is some content coming form app.js',
+    dev:"sachin"
   })
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    message:'Help is on its way, Please wait...'
+    title: 'Help',
+    message:'Help is on its way, Please wait...',
+    dev:"sachin"
   })
 })
 
@@ -38,6 +46,22 @@ app.get('/weather', (req, res) => {
     Location: 'Bangalore',
     temperature: '30 deg',
     chancesOfRain: '50%'
+  })
+})
+
+app.get('/help/*',(req,res)=>{
+  res.render('404',{
+    title:'4.0.4',
+    message:'Help Article not found!!!',
+    dev:'Sachin'
+  })
+})
+
+app.get('*',(req,res)=>{
+  res.render('404',{
+    title:'4.0.4',
+    message:'Please check the url',
+    dev:'Sachin'
   })
 })
 
